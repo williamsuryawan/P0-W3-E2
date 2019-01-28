64f8742cb0446090524e9b85009ab38177d471d8
William Suryawan <suryawan.william@gmail.com>
2019-01-28 19:22:08 +0700

    revisi file

--- William-P0-W3-E2.js
+++ William-P0-W3-E2.js
@@ -1,6 +1,9 @@
-var string ="Hello World!";
+function array1 (string) {
 var jawaban ="";
 for (var i=string.length-1; i>=0;i--){
   jawaban=jawaban+string[i];
 }
-console.log(jawaban);
+return jawaban
+}
+
+console.log(array1("Hello World!"));
